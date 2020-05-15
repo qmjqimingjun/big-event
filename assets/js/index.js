bb6b5ce5a07eeba1b06a43ff3d4d106d569c4dc4
qimingjun <927416232@qq.com>
2020-05-15 21:02:51 +0800

    完成了退出功能

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+$(function () {
+  //-------------获取用户信息并渲染到页面-------------------------
+  getUserInfo();
+  //-------------------------退出功能-------------------------
+  $('#logout').click(function () {
+    layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+      layer.close(index);
+    });
+  });
+});
+//封装函数完成获取用户信息并渲染到页面中
+function getUserInfo() {
+  $.ajax({
+    type: 'get',
+    url: 'http://www.liulongbin.top:3007/my/userinfo',
+    headers: {
+      Authorization: localStorage.getItem('token'),
+    },
+    success: function (res) {
+      if (res.status == 0) {
+        var name = res.data.nickname || res.data.username;
+        $('.welcome').html('欢迎你 &nbsp;' + name);
+        if (res.data.user_pic) {
+          $('.layui-nav-img').attr('src', res.data.user_pic).show();
+          $('.text-img').hide();
+        } else {
+          $('.layui-nav-img').hide();
+          $('.text-img')
+            .css('display', 'inline-block')
+            .text(name.substr(0, 1).toUpperCase());
+        }
+      }
+    },
+  });
+}
